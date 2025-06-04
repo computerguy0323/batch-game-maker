e514daeef2528e33023469423fcf6ae4b86d6fdc
DESKTOP-B7M523H\Nolan Felder <computerguy0323@hotmail.com>
2025-06-04 19:49:30 -0400

    Added the files themselves!

--- gitpush.bat
+++ gitpush.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d "%~dp0"
-git status
+
+echo Pulling latest changes from remote (may ask to merge unrelated histories)...
+git pull origin main --allow-unrelated-histories
+
 set /p msg="Enter commit message: "
 
 git add .
