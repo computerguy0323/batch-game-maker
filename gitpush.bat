f92f19507ebeffd7a1d1cedbdb0689c25f91d7ab
DESKTOP-B7M523H\Nolan Felder <computerguy0323@hotmail.com>
2025-06-04 19:41:48 -0400

    test

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,11 @@
 @echo off
-REM Set your commit message
+cd /d "%~dp0"
+git status
 set /p msg="Enter commit message: "
 
-REM Add all changes
 git add .
-
-REM Commit with the message you typed
 git commit -m "%msg%"
 
-REM Push to the main branch
 git push origin main
 
 pause
